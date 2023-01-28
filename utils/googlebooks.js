10a50a68e1125e2b687db2615aa034774c095759
addisongarza <114454304+addisongarza@users.noreply.github.com>
2023-01-28 13:03:08 -0700

    Update googlebooks.js
    This code exports a function that takes a phrase as a parameter and makes a GET request to the Google Books API with the provided phrase as a query parameter.
    It then creates a formatted object with data, status, statusText, headers, requestHeader of the response.
    It then returns the formatted object. The function also handle any error that may occur during the request by logging it to the console.

--- utils/googlebooks.js
+++ utils/googlebooks.js
@@ -1,24 +1,28 @@
 const axios = require('axios')
 
 module.exports = async (phrase) => {
-    const results = await axios.get('https://www.googleapis.com/books/v1/volumes', {
-        params: {
-            format: 'json',
-            q: `${phrase}`
+    try {
+        const results = await axios.get('https://www.googleapis.com/books/v1/volumes', {
+            params: {
+                format: 'json',
+                q: `${phrase}`
+            }
+        });
+
+        // Create a formatted object
+        var formattedObj = {
+            data: results.data,
+            status: results.status,
+            statusText: results.statusText,
+            headers: results.headers,
+            requestHeader: results.config.headers
         }
-    })
 
-
-    //
-    //TODO: return a formattedObj
-    var formattedObj = {
-        data: results.data,
-        status: results.status,
-        statusText: results.statusText,
-        headers: results.headers,
-        requestHeader: results.config.headers
+        // Return the formatted object
+        return formattedObj;
+    } catch (error) {
+        // Handle any error that may occur
+        console.error(error);
     }
-
-    //TODO: return the formattedObject
-    return JSON.stringify(results.data);
 }
+
